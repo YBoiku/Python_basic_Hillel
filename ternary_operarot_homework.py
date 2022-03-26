bd6e2e4b7954a7275eeacc42a7d43e2f5d348ac2
yehor <yehorboiku@gmail.com>
2022-03-26 20:03:26 +0200

    Тернарные операторы

--- ternary_operarot_homework.py
+++ ternary_operarot_homework.py
@@ -1,8 +1,20 @@
 ###################### 1 ######################
-value = 150
-new_value = int(value) / 2 if value < 100 else int(-value)
+value = 10
+new_value = value / 2 if value < 100 else -value
 print(new_value)
 ###################### 2 ######################
-value = 150
-new_value = int(value) == "1" if value < 100 else int(value) == "0"
+value = 15
+new_value = 1 if value < 100 else 0
 print(new_value)
+###################### 3 ######################
+value = 150
+new_value = True if value < 100 else False
+print(new_value)
+###################### 4 ######################
+my_str = "qwer"
+new_str = my_str + my_str if len(my_str) < 5 else my_str
+print(new_str)
+###################### 5 ######################
+my_str = "qwer"
+new_str = my_str + my_str[::-1] if len(my_str) < 5 else my_str
+print(new_str)
