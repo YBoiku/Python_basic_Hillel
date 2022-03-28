63b6d57088a7a05b7db0abcf8928b9117714be3d
yehor <yehorboiku@gmail.com>
2022-03-29 00:32:23 +0300

    Улучшенная версия калькулятора

--- Homework_Calcularot_v.2.1.py
+++ Homework_Calcularot_v.2.1.py
@@ -3,27 +3,27 @@ while True:
                        "\n5: Возведение в степень\n6: Квадратный корень из числа\nВыбор операции: ")
     if input_case in "123456":
         try:
-            if input_case == 1:
+            if input_case == "1":
                 value_1 = float(input("Первое слагаемое: "))
                 value_2 = float(input("Второе слагаемое: "))
                 result = value_1 + value_2
-            elif input_case == 2:
+            elif input_case == "2":
                 value_1 = float(input("Первое уменьшаемое: "))
                 value_2 = float(input("Второе вычитаемое: "))
                 result = value_1 - value_2
-            elif input_case == 3:
+            elif input_case == "3":
                 value_1 = float(input("Первый множитель: "))
                 value_2 = float(input("Второй множитель: "))
                 result = value_1 * value_2
-            elif input_case == 4:
+            elif input_case == "4":
                 value_1 = float(input("Первое делимое: "))
                 value_2 = float(input("Второй делитель: "))
                 result = value_1 / value_2
-            elif input_case == 5:
+            elif input_case == "5":
                 value_1 = float(input("Первый основание: "))
                 value_2 = float(input("Второй показатель степени: "))
                 result = value_1 ** value_2
-            elif input_case == 6:
+            elif input_case == "6":
                 value_1 = float(input("Число под корнем: "))
                 result = value_1 ** 0.5
             else:
@@ -35,7 +35,6 @@ while True:
             print("Нельзя делить на ноль")
     else:
         print("Необходимо выбрать одну из перечисленных возможностей")
-
 
     new_start = input("Хотите посчитать еще раз? Любая кнопка/N: ").lower()
     if new_start == "n":
