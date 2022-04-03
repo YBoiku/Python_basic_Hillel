4961b79ead2764ccff1d0a9c66e7f8298b7853b4
yehor <yehorboiku@gmail.com>
2022-04-03 18:38:35 +0300

    Nested Loops

--- Homework_6.py
+++ Homework_6.py
@@ -5,11 +5,11 @@ for some_list in my_list:
         print(some_list)
 ####################### 2 #######################
 my_list = [100, 156, 365, 4568, 20, 48, 600]
-new_list = []
-for some_list in my_list:
-    if some_list > 100:
-        new_list.append(some_list)
-print(new_list)
+my_result = []
+for new_list in my_list:
+    if new_list > 100:
+        my_result.append(new_list)
+print(my_result)
 ####################### 3 #######################
 my_list = [100, 1, 365, 48, 2, 48, 60]
 # my_list = [1]
