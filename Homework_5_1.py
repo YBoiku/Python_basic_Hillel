8da99f18994e2d61c4a2f73ad31966cc44f8a639
yehor <yehorboiku@gmail.com>
2022-04-09 14:19:05 +0300

    Homework 5

--- Homework_5_1.py
+++ Homework_5_1.py
@@ -9,15 +9,14 @@
 # len_rev_number = len(str(int(str(number)[::-1])))
 # count_zero_back = len_orig_number - len_rev_number
 # print(count_zero_back)
+
 # count_zero_back = len(str(number)[::-1]) - len(str(int(str(number)[::-1])))
 ################# 3 #################
 # my_list_1 = [1, 2, 3, 4, 5, 7, "house", 8, 9, 15, "qwerty", "apple"]
 # my_list_2 = [1, 2, 3, 4, 5, 156, "cat", "dog"]
 # my_result = []
-#
 # my_range_1 = my_list_1[1::2]
 # my_range_2 = my_list_2[0::2]
-#
 # for index in my_range_1:
 #     my_result.append(index)
 # for index1 in my_range_2:
@@ -25,18 +24,13 @@
 # print(my_result)
 # Код работает, но уверен можно легче написать, но другого решения найти не смог. Сможете подсказать с правильным?
 ################# 4 #################
-
-
 # my_list = [1, 2, 3, 4]
 # new_list = []
 # range_list = my_list[1:]
-#
 # for create_list in range_list:
 #     new_list.append(create_list)
 # new_list.append(my_list[0])
 # print(new_list)
-
-
 ################# 5 #################
 # my_list = [1, 2, 3, 4]
 # my_list += [my_list.pop(0)]
@@ -55,14 +49,14 @@
 # my_str_l = my_str.lower()
 # l_limit = "e"
 # r_limit = "o"
-# # ff_l_split = my_str_l.find(f'{l_limit}')
-# # l_split = my_str[ff_l_split + 1::]
-# # rev_split = l_split[::-1]
-# # ff_r_split = rev_split.find(f'{r_limit}')
-# # r_split = rev_split[ff_r_split + 1::]
-# # end_revers = r_split[::-1]
-# # print(end_revers)
-#
+# ff_l_split = my_str_l.find(f'{l_limit}')
+# l_split = my_str[ff_l_split + 1::]
+# rev_split = l_split[::-1]
+# ff_r_split = rev_split.find(f'{r_limit}')
+# r_split = rev_split[ff_r_split + 1::]
+# end_revers = r_split[::-1]
+# print(end_revers)
+
 # left_split = my_str_l[my_str_l.find(f"{l_limit}") + 1:]
 # rev_split = left_split[::-1]
 # right_split = rev_split[rev_split.find(f"{r_limit}") + 1:]
@@ -85,14 +79,13 @@
 ################# 9 #################!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # some_list = [2, 4, 1, 5, 3, 9, 0, 7]
 # list_s = []
-#
-# for x in some_list:
-#     if some_list.index(x) == 0 or some_list.index(x) == len(some_list) - 1:
+# for each_symbol in some_list:
+#     if some_list.index(each_symbol) == 0 or some_list.index(each_symbol) == len(some_list) - 1:
 #         pass
-#     elif x > some_list[(some_list.index(x))-1] + some_list[(some_list.index(x))+1]:
-#         list_s.append(x)
-#
-# print(list_s)
+#     elif each_symbol > some_list[(some_list.index(each_symbol)) - 1] + some_list[(some_list.index(each_symbol)) + 1]:
+#         list_s.append(each_symbol)
+# print(len(list_s))
+
 # Это единственный способ который я нашел и работал у меня, единственное у него есть нюанс
 # если использовать список с повторяющимеся элементами, то они оба будет выводится, в не зависимости от равенства
 # как пример вот список: some_list = [2, 4, 1, 5, 3, 9, 0, 7, 9, 72]
@@ -117,14 +110,13 @@
 # new_set = my_set_1.intersection(my_set_2)
 # new_list = list(new_set)
 # print(new_set)
-################# 13 #################!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+################# 13 #################
 # my_string_1 = "aaaasdf1"
 # my_string_2 = "asdfff2"
-# some1 = []
-# some2 = []
-# for symbol1 in set(my_string_1):
-#     some1.append(symbol1)
-# print(some1)
-# for symbol2 in set(my_string_2):
-#     some2.append(symbol2)
-# print(some2)
+# new_list = []
+# for some_sym in my_string_1:
+#     factor = my_string_1.find(some_sym) - my_string_1.rfind(some_sym)
+#     if factor == 0:
+#         if some_sym in my_string_2 and my_string_2.find(some_sym) - my_string_2.rfind(some_sym) == 0:
+#             new_list.append(some_sym)
+# print(new_list)
