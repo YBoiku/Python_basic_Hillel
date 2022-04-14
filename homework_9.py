20d45255f79a91ea3d4cb28a7138e3a5113ee8aa
yehor <yehorboiku@gmail.com>
2022-04-14 12:48:07 +0300

    homework 9.2

--- homework_9.py
+++ homework_9.py
@@ -3,75 +3,110 @@ from string import ascii_lowercase
 
 
 ###################### 1 #######################
-def for_reverse_string(original_string):
+def for_reverse_odd_string(original_strings_list):
     reversed_string = []
-    for index_companies_list, companies_list in enumerate(original_string):
-        if index_companies_list % 2 != 0:
-            reversed_string.append(original_string[index_companies_list])
+    for index_string, string_value in enumerate(original_strings_list):
+        if index_string % 2 != 0:
+            reversed_string.append(original_strings_list[index_string])
         else:
-            reversed_company = original_string[index_companies_list][::-1]
+            reversed_company = original_strings_list[index_string][::-1]
             reversed_string.append(reversed_company)
     return reversed_string
 
 
+my_list = ["N-iX", "AUTO DOC", "EPAM", "Sigma Software", "Ciklum", "DataArt", "Luxoft", "Genesis"]
+new_list = for_reverse_odd_string(my_list)
+
+
 ###################### 2 #######################
-def filter_first_a(some_string):
-    new_list = []
-    for each_word in some_string:
+def filter_first_a(original_strings_list):
+    filter_strings_list = []
+    for each_word in original_strings_list:
         if each_word[0].lower() == "a":
-            new_list.append(each_word)
-    return new_list
+            filter_strings_list.append(each_word)
+    return filter_strings_list
+
+
+my_list = ["N-iX", "AUTO DOC", "EPAM", "Sigma Software", "Ciklum", "DataArt", "Luxoft", "Genesis"]
+new_list = filter_first_a(my_list)
 
 
 ###################### 3 #######################
-def filter_letter_a(some_string):
+def filter_letter_a(original_strings_list):
     filter_string = []
-    for each_word in some_string:
+    for each_word in original_strings_list:
         if "a" in each_word.lower():
             filter_string.append(each_word)
     return filter_string
 
 
+my_list = ["N-iX", "AUTO DOC", "EPAM", "Sigma Software", "Ciklum", "DataArt", "Luxoft", "Genesis"]
+new_list = filter_letter_a(my_list)
+
+
 ###################### 4 #######################
 def filter_for_str(some_list):
-    new_list = []
+    list_result_filter = []
     for str_number in some_list:
         if type(str_number) is str:
-            new_list.append(str_number)
-    return new_list
+            list_result_filter.append(str_number)
+    return list_result_filter
+
+
+my_list = [1, 2, 3, "11", "22", 33]
+new_list = filter_for_str(my_list)
 
 
 ###################### 5 #######################
-def filter_set(some_list):
-    new_list = []
+def unique_symbol_in_string(some_list):
+    list_result_filter = []
     for some_symbols in set(some_list):
         if some_list.count(some_symbols) == 1:
-            new_list.append(some_symbols)
-    return new_list
+            list_result_filter.append(some_symbols)
+    return list_result_filter
+
+
+my_list = [1, 22, 3, 5, 5, 33, 44, 44]
+new_list = unique_symbol_in_string(my_list)
 
 
 ###################### 6 #######################
-def filter_intersection(list_1, list_2):
+def intersection_list_filter(list_1, list_2):
     list_1 = set(list_1)
     list_2 = set(list_2)
     new_set = list_1.intersection(list_2)
     return list(new_set)
 
 
+my_list_1 = [1, 3, 5, 33, 44]
+my_list_2 = [22, 3, 7, 10, 44]
+new_list = (intersection_list_filter(my_list_1, my_list_2))
+
+
 ###################### 7 #######################
-def set_two_string(string_1, string_2):
-    new_list = []
+def unique_common_sym_two_str(string_1, string_2):
+    unique_common_sym = []
     for some_sym in set(string_1):
         if string_1.count(some_sym) == 1 and string_2.count(some_sym) == 1:
-            new_list.append(some_sym)
-    return new_list
+            unique_common_sym.append(some_sym)
+    return unique_common_sym
+
+
+my_string_1 = "aaaasdf1"
+my_string_2 = "asdfff2"
+new_list_1 = unique_common_sym_two_str(my_string_1, my_string_2)
 
 
 ###################### 8 #######################
 def create_email(user_domain, user_name):
-    random_names = choice(user_name)
+    random_name = choice(user_name)
     random_numbers_1 = str(randint(100, 999))
     random_numbers_2 = "".join(sample(list(set(ascii_lowercase)), randint(5, 7)))
-    random_domains = choice(user_domain)
-    random_email = f"{random_names}.{random_numbers_1}@{random_numbers_2}.{random_domains}"
+    random_domain = choice(user_domain)
+    random_email = f"{random_name}.{random_numbers_1}@{random_numbers_2}.{random_domain}"
     return random_email
+
+
+name = ["pink_killer", "grinch", "konstantine", "nibulon777"]
+domain = ["ua", "biz", "org", "coop", "eus", "edu"]
+new_email = create_email(domain, name)
