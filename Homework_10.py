abe9d98ad37f3734709b8a3749bfc2d0af268ae0
yehor <yehorboiku@gmail.com>
2022-04-17 19:30:33 +0300

    homework 10

--- Homework_10.py
+++ Homework_10.py
@@ -3,13 +3,33 @@ def list_of_domains_name_from_file(path_and_filename):
     with open(path_and_filename, 'r') as my_file:
         all_domains = my_file.read()
     domains_names = []
-    for data_line in all_domains.split('\n'):
-        if data_line[0] == '.':
-            domains_names.append(data_line.split('.')[1])
+    for each_data_line in all_domains.split('\n'):
+        if each_data_line[0] == '.':
+            domains_names.append(each_data_line.split('.')[1])
     with open(path_and_filename, 'w') as new_txt_file:
         new_txt_file.write(all_domains)
     return domains_names
 
 
 list_of_domains_names = list_of_domains_name_from_file('Homework text/domains.txt')
+
+
 ######################## 2 ########################
+def list_of_last_names_from_file(path_and_filename):
+    with open(path_and_filename, 'r') as my_file:
+        all_information_from_file = my_file.read()
+    last_names = []
+    for data_line in all_information_from_file.split('\n'):
+        if data_line:
+            each_data_line = data_line.split('\t')
+            last_names.append(each_data_line[1])
+    with open(path_and_filename, 'w') as new_txt_file:
+        new_txt_file.write(all_information_from_file)
+    return last_names
+
+
+last_names_in_file = list_of_last_names_from_file('Homework text/names.txt')
+######################## 3 ########################
+
+
+
