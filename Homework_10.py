ce40e1d157bfe08c76a96d91dc256513e45b5dec
Yboiku <yehorboiku@gmail.com>
2022-04-18 22:23:42 +0300

    -

--- Homework_10.py
+++ Homework_10.py
@@ -29,25 +29,26 @@ def list_of_last_names_from_file(path_and_filename):
 
 
 last_names_in_file = list_of_last_names_from_file('Homework text/names.txt')
-# print(last_names_in_file)
 
 
 ######################## 3 ########################
-def list_of_date_dict_from_file(path_and_filename):
+def list_of_dates_dict_from_file(path_and_filename):
     with open(path_and_filename, 'r') as my_file:
         all_information_from_file = my_file.read()
     dates = []
     for data_line in all_information_from_file.split('\n'):
-        if len(data_line) > 15:
-            only_some_date = data_line.split()[0:3]
-            only_some_date = ' '.join(only_some_date)
-            dates.append({'date': f'{only_some_date}'})
+        only_some_date = data_line.split()[0:3]
+        only_some_date = ' '.join(only_some_date)
+        for split_some_date in only_some_date.split('-'):
+            only_some_date = split_some_date.split()[0:3]
+            if len(only_some_date) > 2:
+                only_some_date = ' '.join(only_some_date)
+                dates.append({'date': f'{only_some_date}'})
     with open(path_and_filename, 'w') as new_txt_file:
         new_txt_file.write(all_information_from_file)
     return dates
 
 
-dates_from_file = list_of_date_dict_from_file('Homework text/authors.txt')
-print(dates_from_file)
-######################## 4 ########################
+dates_from_file = list_of_dates_dict_from_file('Homework text/authors.txt')
 
+######################## 4 ########################
