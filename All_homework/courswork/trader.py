812874e55560ccebf917b4317052c26f11b40dc7
Yboiku <yehorboiku@gmail.com>
2022-05-09 16:00:50 +0300

    09-05-22

--- All_homework/courswork/trader.py
+++ All_homework/courswork/trader.py
@@ -1,77 +1,77 @@
 import json
+import os
 import random
-import typing
+from typing import Optional
+import sys
 
 
-def read_config(file_path: str) -> dict:
-    with open(file_path, 'r') as my_file:
-        dict_with_info = json.load(my_file)
-    return dict_with_info
+class Trader:
+    def __init__(self, path_info: str):
+        self.path_info = path_info
+        self.account_info = self.read_config()
+
+    def read_config(self) -> dict:
+        with open(self.path_info, 'r') as file:
+            dict_with_info = json.load(file)
+        return dict_with_info
+
+    def rate(self):
+        return f"1 USD/ {self.account_info['dollar course']} UAH"
+
+    def available(self):
+        return f"UA account balance: {self.account_info['UA balance']}" \
+               f"\nUSD account balance: {self.account_info['USD balance']}"
+
+    def buy(self, number: Optional[int] = None):
+        need_uah = number * self.account_info['dollar course']
+        if need_uah > self.account_info['UA balance']:
+            return f"UNAVAILABLE, REQUIRED BALANCE UAH {self.account_info['UA balance']}, AVAILABLE {need_uah}"
+        else:
+            actual_uah = self.account_info['UA balance'] - number
+            actual_usd = number / self.account_info['dollar course']
+            self.account_info['UA balance'] = round(actual_uah, 2)
+            self.account_info['USD balance'] += round(actual_usd, 2)
+            return self.account_info
+
+    def sell(self, number: Optional[int] = None):
+        if number > self.account_info['USD balance']:
+            return f"UNAVAILABLE, REQUIRED BALANCE USD {self.account_info['USD balance']}, AVAILABLE {number}"
+        else:
+            actual_usd: int = self.account_info['USD balance'] - number
+            actual_uah: int = number * self.account_info['dollar course']
+            self.account_info['USD balance'] = round(actual_usd, 2)
+            self.account_info['UA balance'] += round(actual_uah, 2)
+            return self.account_info
+
+    def buy_all(self) -> dict:
+        actual_uah: int = 0
+        actual_usd: int = self.account_info['UA balance'] / self.account_info['dollar course']
+        self.account_info['UA balance'] = actual_uah
+        self.account_info['USD balance'] += round(actual_usd, 2)
+        return self.account_info
+
+    def sell_all(self) -> dict:
+        actual_uah = self.account_info['USD balance'] * self.account_info['dollar course']
+        actual_usd = 0
+        self.account_info['UA balance'] += round(actual_uah, 2)
+        self.account_info['USD balance'] = actual_usd
+        return self.account_info
+
+    def next(self) -> dict:
+        actual_course: int = self.account_info['dollar course']
+        delta: int = self.account_info["delta"]
+        max_range_course: int = actual_course + delta
+        min_range_course: int = actual_course - delta
+        new_course = random.uniform(max_range_course, min_range_course)
+        self.account_info['dollar course'] = round(new_course, 2)
+        return self.account_info
+
+    def restart(self):
+        pass
+
+    def write_session_history(self):
+        pass
 
 
-config_file = read_config('config.json')
-
-
-def rate():
-    return f"1 USD/ {config_file['dollar course']} UAH"
-
-
-def available():
-    return f"UA account balance: {config_file['UA balance']}\nUSD account balance: {config_file['USD balance']}"
-
-
-def buy(number: int):
-    need_uah = number * config_file['dollar course']
-    if need_uah > config_file['UA balance']:
-        return f"UNAVAILABLE, REQUIRED BALANCE UAH {config_file['UA balance']}, AVAILABLE {need_uah}"
-    else:
-        actual_uah = config_file['UA balance'] - number
-        actual_usd = number / config_file['dollar course']
-        config_file['UA balance'] = round(actual_uah, 2)
-        config_file['USD balance'] += round(actual_usd, 2)
-        return config_file
-
-
-def sell(number: int):
-    if number > config_file['USD balance']:
-        return f"UNAVAILABLE, REQUIRED BALANCE USD {config_file['USD balance']}, AVAILABLE {number}"
-    else:
-        actual_usd: int = config_file['USD balance'] - number
-        actual_uah: int = number * config_file['dollar course']
-        config_file['USD balance'] = round(actual_usd, 2)
-        config_file['UA balance'] += round(actual_uah, 2)
-        return config_file
-
-
-def buy_all() -> dict:
-    actual_uah: int = 0
-    actual_usd: int = config_file['UA balance'] / config_file['dollar course']
-    config_file['UA balance'] = actual_uah
-    config_file['USD balance'] += round(actual_usd, 2)
-    return config_file
-
-
-def sell_all() -> dict:
-    actual_uah = config_file['USD balance'] * config_file['dollar course']
-    actual_usd = 0
-    config_file['UA balance'] += round(actual_uah, 2)
-    config_file['USD balance'] = actual_usd
-    return config_file
-
-
-def next() -> dict:
-    actual_course: int = config_file['dollar course']
-    delta: int = config_file["delta"]
-    max_range_course: int = actual_course + delta
-    min_range_course: int = actual_course - delta
-    new_course = random.uniform(max_range_course, min_range_course)
-    config_file['dollar course'] = round(new_course, 2)
-    return config_file
-
-
-def restart():
-    pass
-
-
-def write_session_history():
-    pass
+config_file = Trader('config.json')
+# result = config_file.sell(10)
